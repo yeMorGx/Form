dc0fb4d1de6cb5915ec0ed6467af40a7bfe2ba70
Erixk013 <erick.oliveira.domingos587@gmail.com>
2025-11-03 22:23:50 -0300

    Salvando dados 2.0

--- Pages/enviartorcedoraction.php
+++ Pages/enviartorcedoraction.php
@@ -1,9 +1,24 @@
 <?php
-echo "Hello Word";
+echo "<h1>Dados Enviados</h1>";
+echo "<p>Torcedor: " . $_POST['name'] . "</p>";
+//echo "<p>Torcedor: {$_POST['nome']} </p>";
+echo "============================================================================";
+echo "<p> CPF: " . $_POST['CPF'] . "</p>";
+echo "<p> E-mail:" . $_POST['email'] . "</p>";
+echo "<p> Time do coração: " . $_POST['Time'] . "</p>"; 
+echo "<p> A quanto tempo você é sócio: " . $_POST['p1'] . "</p>";
+echo "<p> Você tem camisa oficial: " . $_POST['radio-group2'] . "</p>";
+echo "<p> voce costuma frequentar estádio: " . $_POST['radio-group'] . "</p>";
 
 
-var_dump(value: $_POST);
-var_dump(value: $_GET);
+$frequencia = isset($_POST['p3']) ? $_POST['p3'] : 'Não';
+echo "<p>Frequência: " . $frequencia . "</p>";
+
+$sociotorcedor = isset($_POST['p4']) ? $_POST['p4'] : 'Não';
+echo "<p>Está satisfeito com o pacote de Sócio Torcedor: " . $sociotorcedor . "</p>";
+
+
+
 
 
 
